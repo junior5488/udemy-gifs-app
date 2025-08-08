e800eebcba59c1c10848a2a5546a7d6757fe381d
Guillermo Chavez <junior05.88@gmail.com>
2025-08-08 16:18:37 -0500

    List Json / Input

--- src/app/gifs/pages/gifs-list/gifs-list.component.ts
+++ src/app/gifs/pages/gifs-list/gifs-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, input } from '@angular/core';
 import { GifsListItemComponent } from "./gifs-list-item/gifs-list-item.component";
 
+interface Image {
+  id: number;
+  desc: string;
+}
+
 @Component({
   selector: 'app-gifs-list',
   imports: [GifsListItemComponent],
   templateUrl: './gifs-list.component.html'
 })
 export class GifsListComponent {
-  gifs = input.required<string[]>();
+  gifs = input.required<Image[]>();
 }
--- src/app/gifs/pages/treanding-page/treanding-page.component.ts
+++ src/app/gifs/pages/treanding-page/treanding-page.component.ts
@@ -1,20 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { GifsListComponent } from "../gifs-list/gifs-list.component";
 
-const imageUrls: string[] = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+const imageUrls = [
+    { id: 1, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg'},
+    { id: 2, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg'},
+    { id: 3, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg'},
+    { id: 4, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg'},
+    { id: 5, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg'},
+    { id: 6, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg'},
+    { id: 7, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg'},
+    { id: 8, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg'},
+    { id: 9, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg'},
+    { id: 10, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg'},
+    { id: 11, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg'},
+    { id: 12, desc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg'}
 ];
+
 
 @Component({
   selector: 'app-treanding-page',
@@ -24,5 +25,5 @@ const imageUrls: string[] = [
   templateUrl: './treanding-page.component.html',
 })
 export default class TreandingPageComponent {
-  gifs = signal<string[]>(imageUrls);
+  gifs = signal(imageUrls);
 }
